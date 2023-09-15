31909d1eb11748bcf6d5a79f5ca4295fea0757d0
Henrique Szabo <118364512+Szaboltz@users.noreply.github.com>
2023-09-15 11:12:31 -0300

    Add files via upload

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,19 +2,18 @@ var ct = 1;
 var output = [];
 
 function fizzBuzz() {
-  if (ct % 3 === 0 && ct % 5 === 0) {
-    output.push("fizzBuzz");
-  } else if (ct % 3 === 0) {
-    output.push("fizz");
-  } else if (ct % 5 === 0) {
-    output.push("buzz");
-  } else {
-    output.push(ct);
+  while (ct < 100) {
+    if (ct % 3 === 0 && ct % 5 === 0) {
+      output.push("fizzBuzz");
+    } else if (ct % 3 === 0) {
+      output.push("fizz");
+    } else if (ct % 5 === 0) {
+      output.push("buzz");
+    } else {
+      output.push(ct);
+    }
+    ct++
   }
-
-  ct++;
-
   console.log(output);
 }
-
 fizzBuzz();
